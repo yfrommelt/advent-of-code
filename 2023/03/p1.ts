140e8f81129e7be4d5b34b43897f2e4bf846857b
Yoann Frommelt <yoann.frommelt@bedrockstreaming.com>
2023-12-03 21:10:51 +0100

    feat: add 2023-03 p2

--- 2023/03/p1.ts
+++ 2023/03/p1.ts
@@ -1,4 +1,5 @@
 import input from './input.txt'
+import {type Col, getPartIdx, getSurroundNumbers, type Grid} from "./shared.ts";
 
 console.log(solve(input))
 
@@ -6,18 +7,6 @@ export function solve(input: string): number {
     const grid = parseGrid(input)
     const partsNumbers = getPartsNumbers(grid)
     return partsNumbers.reduce((sum, partNumber) => sum + partNumber, 0)
-}
-
-type Grid = Row[]
-
-type Row = Col[]
-
-type Col = {
-    rowIdx: number
-    colIdx: number
-    value: string
-    isNumber: boolean
-    isSpecialChar: boolean
 }
 
 function parseGrid(input: string): Grid {
@@ -30,7 +19,7 @@ function parseGrid(input: string): Grid {
                 colIdx,
                 value: col,
                 isNumber: Number.isInteger(Number(col)),
-                isSpecialChar: Boolean(col.match(/[^\d.]/)),
+                isGearChar: Boolean(col.match(/[^\d.]/)),
             }
         })
     })
@@ -54,50 +43,3 @@ function getPartsNumbers(grid: Grid): number[] {
     return partsIdx.map(partIdx => Number(partIdx.split('-').at(2)))
 }
 
-function getSurroundNumbers(grid: Grid, col: Col): Col[] {
-    if (!col.isSpecialChar) {
-        return []
-    }
-    const {rowIdx, colIdx} = col
-
-    const output: Col[] = []
-    for (let r = rowIdx - 1; r <= rowIdx + 1; r++) {
-        for (let c = colIdx - 1; c <= colIdx + 1; c++) {
-            if (r in grid && c in grid[r] && grid[r][c].isNumber) {
-                output.push(grid[r][c])
-            }
-        }
-    }
-
-    return output
-}
-
-
-function getPartIdx(grid: Grid, col: Col): string {
-    const row = grid[col.rowIdx]
-    let partNumber = col.value
-    let firstColIdx = col.colIdx
-
-    let colIdx = col.colIdx - 1
-    while (colIdx !== -1) {
-        if (colIdx in row && row[colIdx].isNumber) {
-            partNumber = row[colIdx].value + partNumber
-            firstColIdx = row[colIdx].colIdx
-            colIdx--
-        } else {
-            colIdx = -1
-        }
-    }
-
-    colIdx = col.colIdx + 1
-    while (colIdx !== -1) {
-        if (colIdx in row && row[colIdx].isNumber) {
-            partNumber = partNumber + row[colIdx].value
-            colIdx++
-        } else {
-            colIdx = -1
-        }
-    }
-
-    return [col.rowIdx, firstColIdx, partNumber].join('-')
-}
---
+++ 2023/03/p2.ts
@@ -0,0 +1,74 @@
+import input from './input.txt'
+import {type Col, getPartIdx, getSurroundNumbers, type Grid} from "./shared.ts";
+
+console.log(solve(input))
+
+export function solve(input: string): number {
+    const grid = parseGrid(input)
+    const gearsRatio = getGearsRatio(grid)
+    return gearsRatio.reduce((sum, partNumber) => sum + partNumber, 0)
+}
+
+function parseGrid(input: string): Grid {
+    const grid: Grid = []
+    input.split(/\r?\n/).forEach((row, rowIdx) => {
+        grid[rowIdx] = []
+        row.split('').forEach((col, colIdx) => {
+            grid[rowIdx][colIdx] = {
+                rowIdx,
+                colIdx,
+                value: col,
+                isNumber: Number.isInteger(Number(col)),
+                isGearChar: col === '*',
+            }
+        })
+    })
+    return grid
+}
+
+function getGearsRatio(grid: Grid): number[] {
+    let surroundingNumbers: Col[] = []
+    grid.forEach(row => {
+        row.forEach(col => {
+            surroundingNumbers = surroundingNumbers.concat(filterSurroundNumbers(getSurroundNumbers(grid, col), grid))
+        })
+    })
+
+    let partsIdx: string[] = []
+    surroundingNumbers.forEach(col => {
+        partsIdx = partsIdx.concat(getPartIdx(grid, col))
+    })
+    partsIdx = [...(new Set([...partsIdx]))]
+
+    const output: number[] = []
+    for (let i = 0; i < partsIdx.length - 1; i = i + 2) {
+        const part1Idx = partsIdx[i]
+        const part2Idx = partsIdx[i + 1]
+        output.push(Number(part1Idx.split('-').at(2)) * Number(part2Idx.split('-').at(2)))
+    }
+
+    return output
+}
+
+function filterSurroundNumbers(surroundingNumbers: Col[], grid: Grid): Col[] {
+    // not a gear if there is not two parts
+    if (surroundingNumbers.length < 2) {
+        return []
+    }
+
+    // parts in two different row
+    const rowsIdx = surroundingNumbers.map(col => col.rowIdx)
+    if (Math.min(...rowsIdx) !== Math.max(...rowsIdx)) {
+        return surroundingNumbers
+    }
+
+    // parts in same row but seperated
+    const colsIdx = surroundingNumbers.map(col => col.colIdx)
+    const rowIdx = surroundingNumbers.at(0)?.rowIdx ?? 0
+    const centerCol = grid[rowIdx][Math.min(...colsIdx) + 1]
+    if (Math.max(...colsIdx) - Math.min(...colsIdx) > 1 && !centerCol.value.match(/\d/)) {
+        return surroundingNumbers
+    }
+
+    return []
+}
